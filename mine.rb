379d9b8e2837bdbdae6369ac84549c5d72fb25ce
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-27 04:38:22 +0000

    Automatically backed up by Learn

--- mine.rb
+++ mine.rb
@@ -9,7 +9,8 @@ while row_index < my_books.count do
   element_index=0
   while element_index<my_books[row_index].count do
     p my_books[row_index][element_index]
-  end
     element_index +=1
+  end
+    row_index +=1
   end
 end
