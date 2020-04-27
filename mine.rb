8756ee516bd251cda68b24bb5c0f6e4792cd33c1
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-27 04:37:48 +0000

    Automatically backed up by Learn

---
+++ mine.rb
@@ -0,0 +1,16 @@
+my_books = [
+  ["teksas","tommiks"],
+  ["risale","cevsen",'Kuran'],
+  ["math","science"]
+]
+
+row_index = 0
+while row_index < my_books.count do
+  element_index=0
+  while element_index<my_books[row_index].count do
+    p my_books[row_index][element_index]
+  end
+    element_index +=1
+  end
+end
+  
